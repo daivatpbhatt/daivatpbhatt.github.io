f9c7d1e003d032013ee28400df73eaec3dcbe9b7
daivatpbhatt <daivatpbhatt@gmail.com>
2017-08-16 22:02:08 +0530

    Summer Project javascript

--- projects-js.js
+++ projects-js.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function($) {
     $("#semester_2").slideToggle("slow");
     $("#semester_3").slideUp();
     $("#semester_4").slideUp();
+    $("#summer_16").slideUp();
   });
 
   $("#semester_2_js").click(function() {
@@ -11,6 +12,7 @@ jQuery(document).ready(function($) {
     $("#semester_2").slideToggle("slow");
     $("#semester_3").slideUp();
     $("#semester_4").slideUp();
+    $("#summer_16").slideUp();
   });
 
   $("#semester_3_js").click(function() {
@@ -18,6 +20,7 @@ jQuery(document).ready(function($) {
     $("#semester_2").slideUp();
     $("#semester_3").slideToggle("slow");
     $("#semester_4").slideToggle("slow");
+    $("#summer_16").slideUp();
   })
 
   $("#semester_4_js").click(function() {
@@ -25,7 +28,15 @@ jQuery(document).ready(function($) {
     $("#semester_2").slideUp();
     $("#semester_3").slideToggle("slow");
     $("#semester_4").slideToggle("slow");
+    $("#summer_16").slideUp();
   })
 
+  $("#summer_16_js").click(function(){
+    $("#semester_1").slideUp();
+    $("#semester_2").slideUp();
+    $("#semester_3").slideUp();
+    $("#semester_4").slideUp();
+    $("#summer_16").slideToggle("slow");
+  })
 });
 
